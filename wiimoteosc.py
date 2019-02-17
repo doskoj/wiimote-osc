e18e053a52c07c1ebc02b45c5d7bf139f1783de4
root <doskoj@rpi.edu>
2019-02-17 18:07:04 +0000

    Fixed issues with imports

--- wiimoteosc.py
+++ wiimoteosc.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python
 
 import cwiid
-from python-osc import udp_client
-from python-osc import osc_message_builder
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
 import time
 import socket
 
@@ -18,12 +18,12 @@ while not wm:
         client.send_message("/wiimote", "Error Opening Wiimote Connection")
         pritn("Error Opening Wiimote Connection")
 
-wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_EXT
 
 while True:
     print(wm.state)
-    client.send_message("/wiimote/buttons", wm.state['buttons'])
-    if (wm.state['buttons'] & cwiid.BTN_1):
-        client.send_message("/wiimote/buttons/1", 1)
-    client.send_message("/wiimote/acc", wm.state['acc'])
+    #client.send_message("/wiimote/buttons", wm.state['buttons'])
+    #if (wm.state['buttons'] & cwiid.BTN_1):
+        #client.send_message("/wiimote/buttons/1", 1)
+    #client.send_message("/wiimote/acc", wm.state['acc'])
     time.sleep(0.1)
